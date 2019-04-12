ff5e4853f72adaa46abe71620c30ad62f56e3a0d
cuonghx <cuonghx@cuonghxs-MacBook-Pro.local>
2019-04-12 15:31:55 +0700

    basic lesson 5

---
+++ TrainningIOS/TrainningIOS/MyTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  MyTableViewCell.swift
+//  TrainningIOS
+//
+//  Created by cuonghx on 4/12/19.
+//  Copyright © 2019 cuonghx. All rights reserved.
+//
+
+import UIKit
+
+class MyTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var label: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
